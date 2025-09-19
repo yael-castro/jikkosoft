ec9cd25e7168ea41079e6d45ae10e603d2403dee
Yael Castro <yy.lgnd@gmail.com>
2025-09-19 12:49:13 -0600

    chore: Removed unused files

--- 3/pkg/jsont/jsont.go
+++
@@ -1,8 +0,0 @@
-package jsont
-
-type User struct {
-	ID    int64  `json:"id,omitempty"`
-	Name  string `json:"name,omitempty"`
-	Email string `json:"email,omitempty"`
-	Age   uint8  `json:"age,omitempty"`
-}
